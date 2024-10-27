30bd255ddb445e2c18c9bcf50f93bb6b966de950
CynyuS <cynyu.shao@gmail.com>
2024-10-27 13:02:42 -0400

    Implement noscale for image.

---
+++ INSTALL.md
@@ -0,0 +1,9 @@
+# Installation
+
+## Using the bogue opam package
+
+The GCS-OCaml requires bogue to be installed for the GUI to run smoothly. To install bogue, run the below command in your terminal.
+
+```
+opam install bogue
+```
--- bin/main.ml
+++ bin/main.ml
@@ -4,14 +4,12 @@ module W = Widget
 module L = Layout
 
 let gcs_map =
-  let p = Image.create ~noscale:false ~bg:Draw.(opaque white) "map.png" in
-  let map_background = Style.create ~background:(Style.image_bg p) () in
-  let map_box = W.box ~w:600 ~h:600 ~style:map_background () in
-  L.resident ~w:900 ~h:600 ~background:(L.style_bg map_background) map_box
+  let map = W.image ~noscale:true "map.png" in
+  L.resident map
 
 let () =
   let menu = L.flat_of_w [ W.text_display "Menu" ] in
-  (* let container = L.make_clip ~h:300 long in *)
-  let layout = L.flat [ menu; gcs_map ] in
+  let map_scroll = L.make_clip ~w:900 ~h:600 gcs_map in
+  let layout = L.flat [ menu; map_scroll ] in
   let gcs = Bogue.of_layout layout in
   Bogue.run gcs
