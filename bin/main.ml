988e895074520fae5ed7c0ccd6f235165be9d568
CynyuS <cynyu.shao@gmail.com>
2024-12-04 15:40:27 -0500

    Implement horizontal scrolling - added an offset reference for the x position - implemented updates to update_map_layout - implemented function update_offset and apply_offset - created new scroll bar widget

--- bin/main.ml
+++ bin/main.ml
@@ -11,6 +11,9 @@ let map = ref (L.resident (W.image !map_file))
 let waypoints = ref empty
 let wp_table = ref (L.resident (W.label "No Waypoints"))
 
+(* horizontal scroll offset reference *)
+let offset_x = ref 0
+
 (* map options *)
 let map_options = [| "Cornell University"; "Ovid Airport"; "Mojave Desert" |]
 let map_files = [| "data/cornell.png"; "data/ovid.png"; "data/mojave.png" |]
@@ -35,16 +38,30 @@ let add_waypoint (x, y) =
   waypoints := append wp !waypoints;
   flush stdout
 
+let update_offset new_offset =
+  offset_x := new_offset;
+  L.setx !map !offset_x
+
 let update_map_layout () =
   let waypoint_icons =
-    Array.to_list (Array.map (fun wp -> wp_icon (coords wp)) !waypoints)
+    Array.to_list
+      (Array.map
+         (fun wp ->
+           let x, y = coords wp in
+           (* Adjust the waypoint's position based on the map's offset *)
+           wp_icon (x + !offset_x, y))
+         !waypoints)
   in
   let combined_layout =
-    L.superpose (!map :: plane_icon (150, 300) :: waypoint_icons)
+    L.superpose (!map :: plane_icon (150 + !offset_x, 300) :: waypoint_icons)
   in
   match !map_layout_ref with
   | Some layout -> L.set_rooms layout [ combined_layout ]
   | None -> ()
+
+let apply_offset () =
+  L.setx !map !offset_x;
+  update_map_layout ()
 
 let update_wp_table () =
   (wp_table :=
@@ -84,12 +101,15 @@ let update_map file =
   map_file := file;
   map := L.resident (W.image !map_file);
 
+  (* if the map is a new one, keep the offset to 0 *)
+  update_offset 0;
+
   W.on_click (L.widget !map) ~click:(fun _ ->
       let x, y = Mouse.pos () in
       let map_x, map_y = (L.xpos !map, L.ypos !map) in
       add_waypoint (x - map_x, y - map_y);
       update_wp_table ();
-      update_map_layout ());
+      apply_offset ());
 
   update_wp_table ();
   update_map_layout ()
@@ -126,8 +146,24 @@ let init_app () =
   let wp_table_super = L.superpose [ !wp_table ] in
   wp_table_ref := Some wp_table_super;
 
+  let map_width = fst (W.size (L.widget !map)) in
+  let slider_max = if map_width > 900 then map_width - 900 else 0 in
+
+  let slider =
+    W.slider_with_action ~value:0 ~length:900 ~thickness:20 ~step:1
+      ~kind:Slider.Horizontal
+      ~action:(fun value ->
+        offset_x := -value;
+        apply_offset ())
+      slider_max
+  in
+
+  let slider_layout = L.resident slider in
+
+  let map_with_slider = L.tower [ map_scroll; slider_layout ] in
+
   let sidebar = L.tower [ map_menu; clear_path_button; wp_table_super ] in
-  let main_layout = L.flat ~sep:20 [ sidebar; map_scroll ] in
+  let main_layout = L.flat ~sep:20 [ sidebar; map_with_slider ] in
   Bogue.of_layout main_layout
 
 (** entry point *)
